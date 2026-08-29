b155ee6b5572128c8c85212fac667098cc0768f0
Nestor CQ <checcanestor@gmail.com>
2026-08-28 22:22:29 -0500

    Modifique los atributos de la clase

---
+++ src/StudentModel.java
@@ -0,0 +1,10 @@
+public class StudentModel {
+    // Modifique la clase del estudiante
+    private String name;
+    private String id;
+
+    public StudentModel(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
