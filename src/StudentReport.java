b0a24f383981db1d0f7db32727733e9aeb80f3f5
Yeferson Rodrigo Quispe Alcasihuincha <yeferson.quispe@tecsup.edu.pe>
2026-08-28 23:29:42 -0500

    Actualice  StudentReport

--- src/StudentReport.java
+++ src/StudentReport.java
@@ -1,5 +1,11 @@
 public class StudentReport {
+
     public void controller() {
         System.out.println("Reporte de estudiantes generado");
+    }
+
+    public void mostrarEstudiante(String nombre, double nota) {
+        System.out.println("Estudiante: " + nombre);
+        System.out.println("Nota final: " + nota);
     }
 }
